499bf861ab7d84d0b300efca4893c7509b3ac060
dan <742348+ProductiveRage@users.noreply.github.com>
2013-03-30 19:11:31 +0000

    Updated FullTextIndexer references and rewrote the search term highlighting code to make better use of the source locations (the resulting code is easier to follow)

--- Blog/Helpers/Posts/PostHelper.cs
+++ Blog/Helpers/Posts/PostHelper.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
 using BlogBackEnd.Caching;
+using BlogBackEnd.FullTextIndexing;
 using BlogBackEnd.Models;
 using FullTextIndexer.Common.Lists;
 using FullTextIndexer.Core.Indexes;
@@ -207,282 +208,104 @@ namespace Blog.Helpers.Posts
 			if (cache == null)
 				throw new ArgumentNullException("cache");
 
-			// Try to determine which segments of the content should be highlighted as matched search terms
 			var plainTextPostContent = GetPostAsPlainText(post, cache);
-			var segmentMatchRegions = IdentifySearchTerms(
+			var matchesToHighlight = SearchTermHighlighter.IdentifySearchTermsToHighlight(
 				plainTextPostContent,
-				sourceLocations,
 				maxLength,
+				sourceLocations,
 				new SearchTermBestMatchComparer()
 			);
-			if (!segmentMatchRegions.Any())
+
+			int startIndexOfContent;
+			if ((plainTextPostContent.Length <= maxLength) || !matchesToHighlight.Any())
+				startIndexOfContent = 0;
+			else
 			{
-				// If none of the search terms matched then just return the original content (have to HtmlEncode the content, even though it's plain
-				// text, in case it contains anything that needs entity-encoding)
-				return (IHtmlString)MvcHtmlString.Create(
-					HttpUtility.HtmlEncode(
-						(plainTextPostContent.Length > maxLength) ? (plainTextPostContent.Substring(0, maxLength) + "...") : plainTextPostContent
+				if (matchesToHighlight.Max(t => t.Index + t.Length) <= maxLength)
+					startIndexOfContent = 0;
+				else
+				{
+					var startIndexOfFirstHighlightedTerm = matchesToHighlight.Min(t => t.Index);
+					if (plainTextPostContent.Length - startIndexOfFirstHighlightedTerm <= maxLength)
+						startIndexOfContent = plainTextPostContent.Length - maxLength;
+					else
+						startIndexOfContent = startIndexOfFirstHighlightedTerm;
+				}
+			}
+
+			// Beginning at this start index, build up a string of content where the highlighted sections are wrapped in "strong" tags (even though the
+			// content is plain text, it will need to be html-encoded since plain text content can include characters that should be encoded such as
+			// "<" or "&")
+			var highlightedContentBuilder = new StringBuilder();
+			var endIndexOfLastSection = startIndexOfContent;
+			var lengthOfContentIncluded = 0;
+			foreach (var matchToHighlight in matchesToHighlight.OrderBy(m => m.Index))
+			{
+				if (matchToHighlight.Index > endIndexOfLastSection)
+				{
+					var unhighlightedContentToAdd = plainTextPostContent.Substring(endIndexOfLastSection, matchToHighlight.Index - endIndexOfLastSection);
+					highlightedContentBuilder.Append(
+						HttpUtility.HtmlEncode(unhighlightedContentToAdd)
+					);
+					lengthOfContentIncluded += unhighlightedContentToAdd.Length;
+				}
+
+				var highlightedContentToAdd = plainTextPostContent.Substring(matchToHighlight.Index, matchToHighlight.Length);
+				highlightedContentBuilder.Append("<strong>");
+				highlightedContentBuilder.Append(
+					HttpUtility.HtmlEncode(highlightedContentToAdd)
+				);
+				highlightedContentBuilder.Append("</strong>");
+				lengthOfContentIncluded += highlightedContentToAdd.Length;
+
+				endIndexOfLastSection = matchToHighlight.Index + matchToHighlight.Length;
+			}
+			
+			// If there's any more content that we can fit into the maxLength constraint after the last highlighted section, then get that too
+			if ((lengthOfContentIncluded < maxLength) && (plainTextPostContent.Length > endIndexOfLastSection))
+			{
+				highlightedContentBuilder.Append(
+					plainTextPostContent.Substring(
+						endIndexOfLastSection,
+						Math.Min(
+							maxLength - lengthOfContentIncluded,
+							plainTextPostContent.Length - endIndexOfLastSection
+						)
 					)
 				);
 			}
 
-			// Otherwise jump to the start of the matched content - ordinarily this will mean jumping to the lowest value in the bestMatch array and
-			// then taking the next maxLength number of characters, but if this would push us past the end of the string then we're better off starting
-			// from the end and taking maxLength BACK from there
-			int numberOfCharactersToTrimFromTheStart, segmentToDisplayLength;
-			if (maxLength > plainTextPostContent.Length)
-			{
-				// This is an easy case; we're able to show the entire content, so no characters need be removed from the start of it
-				numberOfCharactersToTrimFromTheStart = 0;
-				segmentToDisplayLength = plainTextPostContent.Length;
-			}
-			else
-			{
-				// The next case is optimistic; if every match term can be fitted into the first maxLength number of characters
-				// then display the string starting at the beginning
-				var firstCharacterIndex = segmentMatchRegions.Min(s => s.Start);
-				int lastCharacterIndex = 0;
-				foreach (var segment in segmentMatchRegions)
-				{
-					if ((segment.Start + segment.Length) > lastCharacterIndex)
-						lastCharacterIndex = segment.Start + segment.Length;
-				}
-				if (lastCharacterIndex < maxLength)
-				{
-					numberOfCharactersToTrimFromTheStart = 0;
-					segmentToDisplayLength = maxLength;
-				}
-				else
-				{
-					// Otherwise we're going to have to take a segment out of the content
-					if ((firstCharacterIndex + maxLength) > plainTextPostContent.Length)
-						numberOfCharactersToTrimFromTheStart = plainTextPostContent.Length - maxLength;
-					else
-						numberOfCharactersToTrimFromTheStart = firstCharacterIndex;
-					segmentToDisplayLength = maxLength;
-				}
-			}
+			// Add leading and/or trailing ellipses to indicate where content has been skipped over
+			if (startIndexOfContent > 0)
+				highlightedContentBuilder.Insert(0, "..");
+			if ((startIndexOfContent + maxLength) < plainTextPostContent.Length)
+				highlightedContentBuilder.Append("..");
 
-			// Any overlapping regions are combined, then the regions are adjusted by subtracting numberOfCharactersToTrimFromTheStart from the each Start 
-			// value to account for the portion of the content that will be displayed
-			var adjustedSegmentMatchRegions =
-				CombineOverlappingSegments(segmentMatchRegions)
-				.Select(s => new StringSegment(s.Start - numberOfCharactersToTrimFromTheStart, s.Length));
-
-			// This is the section of the plainTextPostContent that will be rendered
-			var plainTextPostContentToShow = plainTextPostContent.Substring(numberOfCharactersToTrimFromTheStart, segmentToDisplayLength);
-			if (plainTextPostContentToShow.Length < plainTextPostContent.Length)
-				plainTextPostContentToShow += "...";
-
-			// The regions are then processed by constructing a new string that takes each section - highlighted and non-highlighted sections - and html
-			// encodes them (so that any characters that require entity-encoding are dealt with correctly) and the highlighted sections being wrapped in
-			// <strong> tags
-			var highlightedContentBuilder = new StringBuilder();
-			var characterIndex = 0;
-			foreach (var segmentToHighlight in adjustedSegmentMatchRegions.OrderBy(s => s.Start))
-			{
-				if (segmentToHighlight.Start > characterIndex)
-				{
-					highlightedContentBuilder.Append(HttpUtility.HtmlEncode(
-						plainTextPostContentToShow.Substring(characterIndex, segmentToHighlight.Start - characterIndex)
-					));
-				}
-				highlightedContentBuilder.Append("<strong>");
-				highlightedContentBuilder.Append(HttpUtility.HtmlEncode(
-					plainTextPostContentToShow.Substring(segmentToHighlight.Start, segmentToHighlight.Length)
-				));
-				highlightedContentBuilder.Append("</strong>");
-				characterIndex = segmentToHighlight.Start + segmentToHighlight.Length;
-			}
-			if (characterIndex < plainTextPostContentToShow.Length)
-				highlightedContentBuilder.Append(HttpUtility.HtmlEncode(plainTextPostContentToShow.Substring(characterIndex)));
-			
 			return (IHtmlString)MvcHtmlString.Create(highlightedContentBuilder.ToString());
 		}
 
-		/// <summary>
-		/// This will try to identify segments of a content string to highlight that correspond to specified search terms. The segments returned will all
-		/// be displayable within a segment of the content that is no longer than the maxLength value - this may result in compromises being made and not
-		/// all terms may have highlight segments returned (feasibly an empty list will be returned if it was not possible to locate and/or match any of
-		/// the terms in the content given the maxLength constraint).
-		/// </summary>
-		private static NonNullImmutableList<StringSegment> IdentifySearchTerms(
-			string plainTextContent,
-			NonNullImmutableList<IndexData_Extensions_PartialMatches.SourceFieldLocationWithTerm> sourceLocations,
-			int maxLength,
-			IComparer<NonNullImmutableList<StringSegment>> bestMatchDeterminer)
+		// Sort the possibilities by weight (in descending order) and then by number of tokens matched (in descending order; the logic being that if a set of fewer
+		// tokens have the same weight as a set with more tokens then the tokens in the smaller set must be of greater weight and so more important). If further
+		// sorting is required then prefere content closer to the start than the end.
+		private class SearchTermBestMatchComparer : IComparer<NonNullImmutableList<SourceFieldLocation>>
 		{
-			if (plainTextContent == null)
-				throw new ArgumentNullException("plainTextContent");
-			if (sourceLocations == null)
-				throw new ArgumentNullException("sourceLocations");
-			if (!sourceLocations.Any())
-				throw new ArgumentException("Empty sourceLocations set specified - invalid");
-			if (bestMatchDeterminer == null)
-				throw new ArgumentNullException("bestMatchDeterminer");
-
-			if (plainTextContent.Trim() == "")
-				return new NonNullImmutableList<StringSegment>();
-
-			// Try to find starting points in the content for each of the search terms, only search terms that appear at the start of end of string or
-			// that are surrounded by whitespace or puncutation are considered in order to match whole words only)
-			// - Only source locations with a SourceFieldIndex of one will be considered as this is where the Post content will be recorded (the
-			//   Title and MarkdownContent are never null or blank and these fields are specified for the first Content Retrievers when the Index
-			//   Generator is constructed so SourceFieldIndex zero will always be Title and SourceFieldIndex one will always be plain text content
-			//   from the MarkdownContent)
-			var searchTermMatches = new List<Tuple<string, List<int>>>();
-			foreach (var postContentSourceLocation in sourceLocations.Where(l => l.SourceFieldIndex == 1))
+			public int Compare(NonNullImmutableList<SourceFieldLocation> x, NonNullImmutableList<SourceFieldLocation> y)
 			{
-				if ((postContentSourceLocation.SourceIndex + postContentSourceLocation.SourceTokenLength) > plainTextContent.Length)
-					continue;
+				if (x == null)
+					throw new ArgumentNullException("x");
+				if (y == null)
+					throw new ArgumentNullException("y");
 
-				var matchedWord = plainTextContent.Substring(postContentSourceLocation.SourceIndex, postContentSourceLocation.SourceTokenLength);
-				var dataToAddTo = searchTermMatches.FirstOrDefault(e => e.Item1 == matchedWord);
-				if (dataToAddTo == null)
-				{
-					searchTermMatches.Add(Tuple.Create(matchedWord, new List<int>()));
-					dataToAddTo = searchTermMatches.Last();
-				}
-				dataToAddTo.Item2.Add(postContentSourceLocation.SourceIndex);
+				var combinedWeightComparisonResult = y.Sum(s => s.MatchWeightContribution).CompareTo(x.Sum(s => s.MatchWeightContribution));
+				if (combinedWeightComparisonResult != 0)
+					return combinedWeightComparisonResult;
+
+				var numberOfTokensComparisonResult = y.Count.CompareTo(x.Count);
+				if (numberOfTokensComparisonResult != 0)
+					return numberOfTokensComparisonResult;
+
+				return x.Min(s => s.SourceIndex).CompareTo(y.Min(s => s.SourceIndex));
 			}
-			if (searchTermMatches.Count == 0)
-				return new NonNullImmutableList<StringSegment>();
-
-			// Generate all permutations of search term match index values, incorporating -1 values so that we include permutations which don't match all
-			// of the words - eg. if searching for "find this" and we located "find" and "this" at positions [ 7, 102 ] and [ 13, 37, 200 ] then come up
-			// with the complete set of permutations:
-			//   [  -1, -1 ], [  -1, 13 ], [  -1, 37 ], [  -1, 200 ],
-			//   [   7, -1 ], [   7, 13 ], [   7, 37 ], [   7, 200 ],
-			//   [ 102, -1 ], [ 102, 13 ], [ 102, 37 ], [ 102, 200 ]
-			// We'll have to use ToArray on the LINQ results to ensure that they are executed now and not lazily, otherwise they will receive the wrong
-			// value for index!
-			var allPermutations = (new[] { -1 }).Concat(searchTermMatches[0].Item2).Select(v => new[] { v }).ToArray();
-			for (var index = 1; index < searchTermMatches.Count; index++)
-			{
-				allPermutations = allPermutations
-					.SelectMany(a11 => (new[] { -1 }).Concat(searchTermMatches[index].Item2).Select(v => a11.Concat(new[] { v }).ToArray()))
-					.ToArray();
-			}
-
-			// Now determine which of these permutations are valid taking into account the maxLength and keep track of the best one
-			var bestMatch = new NonNullImmutableList<StringSegment>();
-			foreach (var permutation in allPermutations)
-			{
-				// Ignore the case where all values are -1 as this means that no match is made, we want the case where at least one word is matched
-				if (permutation.All(v => v == -1))
-					continue;
-
-				// Determine what section of the original content would have to be displayed in order to show all matched words in the current permutation
-				var min = int.MaxValue;
-				var max = 0;
-				for (var index = 0; index < permutation.Length; index++)
-				{
-					var start = permutation[index];
-					if (start == -1)
-						continue;
-
-					if (start < min)
-						min = start;
-					var end = start + searchTermMatches[index].Item1.Length;
-					if (end > max)
-						max = end;
-				}
-
-				// If this section length exceeds maxLength then it's not valid, so skip over it
-				if ((max - min) > maxLength)
-					continue;
-
-				// Otherwise, record it is as bestMatch if the current bestMatch is null or this is an improvement
-				// - A permutation is a better match than an existing match if it has less -1 value, in other words if it manages to match more words
-				var permutationAsStringSegmentsTemp = new List<StringSegment>();
-				for (var index = 0; index < permutation.Length; index++)
-				{
-					if (permutation[index] != -1)
-						permutationAsStringSegmentsTemp.Add(new StringSegment(permutation[index], searchTermMatches[index].Item1.Length));
-				}
-				var permutationAsStringSegments = permutationAsStringSegmentsTemp.ToNonNullImmutableList();
-				if (bestMatchDeterminer.Compare(bestMatch, permutationAsStringSegments) == 1)
-					bestMatch = permutationAsStringSegments;
-			}
-
-			// Note: bestMatch may remain as an empty list if we didn't manage to find any matches (this should only be the case if maxLength
-			// is very short and the only matched words all exceed it in length)
-			return bestMatch;
-		}
-
-		private class SearchTermBestMatchComparer : IComparer<NonNullImmutableList<StringSegment>>
-		{
-			public int Compare(NonNullImmutableList<StringSegment> x, NonNullImmutableList<StringSegment> y)
-			{
-				// If both terms are null then there's nothing in it, if one is null then prefer the non-null value
-				if ((x == null) && (y == null))
-					return 0;
-				else if (y == null)
-					return -1;
-				else if (x == null)
-					return 1;
-
-				// If x matches more terms then prefer x
-				if (x.Count != y.Count)
-					return (x.Count > y.Count) ? -1 : 1;
-
-				// If they match the same number of terms then prefer the one that matches the longest term(s)
-				var xSortedByLength = x.Select(v => v.Length).OrderByDescending(v => v).ToArray();
-				var ySortedByLength = y.Select(v => v.Length).OrderByDescending(v => v).ToArray();
-				for (var index = 0; index < x.Count; index++)
-				{
-					var xN = xSortedByLength[index];
-					var yN = ySortedByLength[index];
-					if (xN > yN)
-						return -1;
-					else if (yN > xN)
-						return 1;
-				}
-
-				// If they match the same number of terms and the matched terms are all of the same lengths then prefer one that starts earlier on in the string
-				return x.Min(v => v.Start).CompareTo(y.Min(v => v.Start));
-			}
-		}
-
-		private static IEnumerable<StringSegment> CombineOverlappingSegments(IEnumerable<StringSegment> segments)
-		{
-			if (segments == null)
-				throw new ArgumentNullException("segments");
-			if (segments.Any(s => s == null))
-				throw new ArgumentException("Null reference encountered in segments set");
-
-			var segmentsCombined = new List<StringSegment>();
-			foreach (var segment in segments.OrderBy(s => s.Start))
-			{
-				var previousSegment = segmentsCombined.Any() ? segmentsCombined.Last() : null;
-				if ((previousSegment == null) || (segment.Start > (previousSegment.Start + previousSegment.Length)))
-				{
-					segmentsCombined.Add(segment);
-					continue;
-				}
-				segmentsCombined.Remove(previousSegment);
-				segmentsCombined.Add(new StringSegment(
-					previousSegment.Start,
-					segment.Length + previousSegment.Length - (segment.Start - previousSegment.Start))
-				);
-			}
-			return segmentsCombined;
-		}
-
-		private class StringSegment
-		{
-			public StringSegment(int start, int length)
-			{
-				if (start < 0)
-					throw new ArgumentOutOfRangeException("start");
-				if (length <= 0)
-					throw new ArgumentOutOfRangeException("length");
-
-				Start = start;
-				Length = length;
-			}
-			public int Start { get; private set; }
-			public int Length { get; private set; }
 		}
 
 		private static string GetPostAsPlainText(Post post, ICache cache)
---
+++ BlogBackEnd/FullTextIndexing/SearchTermHighlighter.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FullTextIndexer.Common.Lists;
+using FullTextIndexer.Core.Indexes;
+
+namespace BlogBackEnd.FullTextIndexing
+{
+	public static class SearchTermHighlighter
+	{
+		/// <summary>
+		/// TODO (inc. non-overlapping of returned segments)
+		/// </summary>
+		public static NonNullImmutableList<StringSegment> IdentifySearchTermsToHighlight(
+			string content,
+			int maxLengthForHighlightedContent,
+			NonNullImmutableList<SourceFieldLocation> sourceLocations,
+			IComparer<NonNullImmutableList<SourceFieldLocation>> bestMatchDeterminer)
+		{
+			if (content == null)
+				throw new ArgumentNullException("content");
+			if (maxLengthForHighlightedContent <= 0)
+				throw new ArgumentOutOfRangeException("maxLengthForHighlightedContent", "must be greater than zero");
+			if (sourceLocations == null)
+				throw new ArgumentNullException("sourceLocations");
+			if (!sourceLocations.Any())
+				throw new ArgumentException("There must be at least one source location specified");
+			if (sourceLocations.Select(s => s.SourceFieldIndex).Distinct().Count() > 1)
+				throw new ArgumentException("All sourceLocations must have the same SourceFieldIndex");
+			if (bestMatchDeterminer == null)
+				throw new ArgumentNullException("bestMatchDeterminer");
+
+			// Sort sourceLocations by index and then length
+			sourceLocations = sourceLocations.Sort((x, y) =>
+			{
+				if (x.SourceIndex < y.SourceIndex)
+					return -1;
+				else if (y.SourceIndex < x.SourceIndex)
+					return 1;
+
+				if (x.SourceTokenLength < y.SourceTokenLength)
+					return -1;
+				else if (y.SourceTokenLength < x.SourceTokenLength)
+					return 1;
+
+				return 0;
+			});
+
+			// Identify all combinations of source locations that can be shown at once without exceeding the maxLengthForHighlightedContent restraint
+			var sourceLocationChains = new NonNullImmutableList<NonNullImmutableList<SourceFieldLocation>>();
+			for (var indexOfFirstSourceLocationInChain = 0; indexOfFirstSourceLocationInChain < sourceLocations.Count; indexOfFirstSourceLocationInChain++)
+			{
+				var sourceLocationChain = new NonNullImmutableList<SourceFieldLocation>();
+				for (var indexOfLastSourceLocationInChain = indexOfFirstSourceLocationInChain; indexOfLastSourceLocationInChain < sourceLocations.Count; indexOfLastSourceLocationInChain++)
+				{
+					var startPoint = sourceLocations[indexOfFirstSourceLocationInChain].SourceIndex;
+					var endPoint = sourceLocations[indexOfLastSourceLocationInChain].SourceIndex + sourceLocations[indexOfLastSourceLocationInChain].SourceTokenLength;
+					if ((endPoint - startPoint) > maxLengthForHighlightedContent)
+						break;
+
+					sourceLocationChain = sourceLocationChain.Add(sourceLocations[indexOfLastSourceLocationInChain]);
+					sourceLocationChains = sourceLocationChains.Add(sourceLocationChain);
+				}
+			}
+
+			// Get the best source location chain, if any (if not, return an empty set) and translate into a StringSegment set
+			if (!sourceLocationChains.Any())
+				return new NonNullImmutableList<StringSegment>();
+			return ToStringSegments(
+				sourceLocationChains.Sort(bestMatchDeterminer).First()
+			);
+		}
+
+		/// <summary>
+		/// TODO
+		/// </summary>
+		public static NonNullImmutableList<StringSegment> IdentifySearchTermsToHighlight(
+			string content,
+			int maxLengthForHighlightedContent,
+			NonNullImmutableList<IndexData_Extensions_PartialMatches.SourceFieldLocationWithTerm> sourceLocations,
+			IComparer<NonNullImmutableList<SourceFieldLocation>> bestMatchDeterminer)
+		{
+			if (sourceLocations == null)
+				throw new ArgumentNullException("sourceLocations");
+
+			return IdentifySearchTermsToHighlight(
+				content,
+				maxLengthForHighlightedContent,
+				sourceLocations
+					.Select(s => new SourceFieldLocation(s.SourceFieldIndex, s.TokenIndex, s.SourceIndex, s.SourceTokenLength, s.MatchWeightContribution))
+					.ToNonNullImmutableList(),
+				bestMatchDeterminer
+			);
+		}
+
+		/// <summary>
+		/// This will translate the source location data into StringSegment instances, combining any overlapping source locations (if none of them overlap then
+		/// there will be the same number of StringSegments are there were SourceFieldLocations, if any do overlap then less StringSegments will be returned)
+		/// </summary>
+		private static NonNullImmutableList<StringSegment> ToStringSegments(NonNullImmutableList<SourceFieldLocation> sourceLocations)
+		{
+			if (sourceLocations == null)
+				throw new ArgumentNullException("sourceLocations");
+			if (!sourceLocations.Any())
+				throw new ArgumentException("must not be empty", "sourceLocations");
+
+			var stringSegments = new NonNullImmutableList<StringSegment>();
+			var sourceLocationsToCombine = new NonNullImmutableList<SourceFieldLocation>();
+			foreach (var sourceLocation in sourceLocations.Sort((x, y) => x.SourceIndex.CompareTo(y.SourceIndex)))
+			{
+				// If the current sourceLocation overlaps with the previous one (or adjoins it) then they should be combined together (if there
+				// isn't a previous sourceLocation then start a new queue)
+				if (!sourceLocationsToCombine.Any() || (sourceLocation.SourceIndex <= sourceLocationsToCombine.Max(s => (s.SourceIndex + s.SourceTokenLength))))
+				{
+					sourceLocationsToCombine = sourceLocationsToCombine.Add(sourceLocation);
+					continue;
+				}
+
+				// If the current sourceLocation marks the start of a new to-highlight segment then add any queued-up sourceLocationsToCombine
+				// content to the stringSegments set..
+				if (sourceLocationsToCombine.Any())
+					stringSegments = stringSegments.Add(new StringSegment(sourceLocationsToCombine));
+
+				// .. and start a new sourceLocationsToCombine list
+				sourceLocationsToCombine = new NonNullImmutableList<SourceFieldLocation>(new[] { sourceLocation });
+			}
+			if (sourceLocationsToCombine.Any())
+				stringSegments = stringSegments.Add(new StringSegment(sourceLocationsToCombine));
+			return stringSegments;
+		}
+
+		public class StringSegment
+		{
+			public StringSegment(NonNullImmutableList<SourceFieldLocation> sourceLocations)
+			{
+				if (sourceLocations == null)
+					throw new ArgumentNullException("sourceLocations");
+				if (!sourceLocations.Any())
+					throw new ArgumentException("must not be empty", "sourceLocations");
+				if (sourceLocations.Select(s => s.SourceFieldIndex).Distinct().Count() > 1)
+					throw new ArgumentException("All sourceLocations must have the same SourceFieldIndex");
+
+				Index = sourceLocations.Min(s => s.SourceIndex);
+				Length = sourceLocations.Max(s => (s.SourceIndex + s.SourceTokenLength) - Index);
+				SourceLocations = sourceLocations;
+			}
+
+			/// <summary>
+			/// This will always be zero or greater
+			/// </summary>
+			public int Index { get; private set; }
+
+			/// <summary>
+			/// This will always be greater than zero
+			/// </summary>
+			public int Length { get; private set; }
+			
+			/// <summary>
+			/// This will never be null or empty
+			/// </summary>
+			public NonNullImmutableList<SourceFieldLocation> SourceLocations { get; private set; }
+		}
+	}
+}
