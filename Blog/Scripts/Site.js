c3cf9de86124aa8d9f81d98e03de34e5b0f0e7d3
dan <742348+ProductiveRage@users.noreply.github.com>
2013-07-09 19:38:00 +0100

    Moved javascript segment from SiteMaster.cshtml into separate file

---
+++ Blog/Scripts/Site.js
@@ -0,0 +1,20 @@
+﻿var $search = $("input.SiteSearch");
+if ($search.length > 0) {
+	$.ajax({
+		url: "@Url.Content("~/AutoComplete.json")",
+		dataType: 'json',
+		success: function (data) {
+			$search.autocomplete(data);
+		}
+	});
+}
+var $content = $("div.Main div.Content");
+$content.find("pre").addClass("prettyprint");
+prettyPrint();
+$("img").each(function() { ApplyWideImageClassAsRequired(this) }).load(function() { ApplyWideImageClassAsRequired(this) });
+function ApplyWideImageClassAsRequired(objEleImage) {
+	var $image = $(objEleImage);
+	if ($image.width() > 500) {
+		$image.addClass("WideImage");
+	}
+}
