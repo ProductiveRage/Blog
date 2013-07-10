c9854d18b1e3f5d9a8ff5d58379e92bae6752cc0
dan <742348+ProductiveRage@users.noreply.github.com>
2013-07-11 00:05:32 +0100

    Wrapped up Site.js in an IIFE

--- Blog/Scripts/Site.js
+++ Blog/Scripts/Site.js
@@ -1,20 +1,22 @@
-﻿var $search = $("input.SiteSearch");
-if ($search.length > 0) {
-	$.ajax({
-		url: "@Url.Content("~/AutoComplete.json")",
-		dataType: 'json',
-		success: function (data) {
-			$search.autocomplete(data);
-		}
-	});
-}
-var $content = $("div.Main div.Content");
-$content.find("pre").addClass("prettyprint");
-prettyPrint();
-$("img").each(function() { ApplyWideImageClassAsRequired(this) }).load(function() { ApplyWideImageClassAsRequired(this) });
-function ApplyWideImageClassAsRequired(objEleImage) {
-	var $image = $(objEleImage);
-	if ($image.width() > 500) {
-		$image.addClass("WideImage");
+﻿(function () {
+	var $search = $("input.SiteSearch");
+	if ($search.length > 0) {
+		$.ajax({
+			url: "/AutoComplete.json", // TODO: need a better way to reference this file since moving this script out of the master page
+			dataType: 'json',
+			success: function (data) {
+				$search.autocomplete(data);
+			}
+		});
 	}
-}
+	var $content = $("div.Main div.Content");
+	$content.find("pre").addClass("prettyprint");
+	prettyPrint();
+	$("img").each(function () { ApplyWideImageClassAsRequired(this) }).load(function () { ApplyWideImageClassAsRequired(this) });
+	function ApplyWideImageClassAsRequired(objEleImage) {
+		var $image = $(objEleImage);
+		if ($image.width() > 500) {
+			$image.addClass("WideImage");
+		}
+	}
+})();
