e121466ab98a1c0f078b13c81d8dfdf3fff8ec86
Dan Roberts <dangger36@gmail.com>
2021-03-09 18:16:29 +0000

    I'd thought that I used single asterisk for bold in my posts but I don't, so this code can be removed

--- BlogBackEnd/Models/MarkdownTransformations.cs
+++ BlogBackEnd/Models/MarkdownTransformations.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using Markdig;
-using Markdig.Parsers.Inlines;
-using Markdig.Renderers;
-using Markdig.Renderers.Html.Inlines;
-using Markdig.Syntax.Inlines;
 
 namespace BlogBackEnd.Models
 {
@@ -13,7 +9,6 @@ namespace BlogBackEnd.Models
     public static class MarkdownTransformations
 	{
 		private static readonly MarkdownPipeline _markdigPipeline = new MarkdownPipelineBuilder()
-			.UseSingleAsteriskBold()
 			.UseSoftlineBreakAsHardlineBreak()
 			.Build();
 
@@ -23,49 +18,6 @@ namespace BlogBackEnd.Models
 				throw new ArgumentNullException("text");
 
 			return Markdown.ToHtml(markdown, _markdigPipeline);
-		}
-
-		/// <summary>
-		/// This will enable single asterisks to indicate bold text (which is not part of the CommonMark specification that Markdig implements by default)
-		/// </summary>
-		private static MarkdownPipelineBuilder UseSingleAsteriskBold(this MarkdownPipelineBuilder pipeline)
-		{
-			pipeline.Extensions.AddIfNotAlready<SingleAsteriskBoldExtension>();
-			return pipeline;
-		}
-
-		private sealed class SingleAsteriskBoldExtension : IMarkdownExtension
-		{
-			public void Setup(MarkdownPipelineBuilder pipeline) => pipeline.InlineParsers.AddIfNotAlready<EmphasisInlineParser>();
-
-			public void Setup(MarkdownPipeline pipeline, IMarkdownRenderer renderer)
-			{
-				var htmlRenderers = (renderer as HtmlRenderer)?.ObjectRenderers;
-				if (htmlRenderers is null)
-					return;
-
-				if (!htmlRenderers.TryFind<EmphasisInlineRenderer>(out var emphasisInlineRenderer))
-				{
-					emphasisInlineRenderer = new EmphasisInlineRenderer();
-					htmlRenderers.Add(emphasisInlineRenderer);
-				}
-				emphasisInlineRenderer.GetTag = GetTagName;
-			}
-
-			private static string GetTagName(EmphasisInline obj)
-			{
-                // The default configuration for EmphasisInlineRenderer asserts that the DelimiterCount will less than or equals to two and so we can make the same assumption here
-                // ^ See https://github.com/xoofx/markdig/blob/e523dfd7f490fb3ee6f35b2a566f4d8d5710e856/src/Markdig/Renderers/Html/Inlines/EmphasisInlineRenderer.cs#L60
-                //
-                // 2021-03-08 DWR: I previousld used "MarkdownSharp" to translate the markdown files into HTML, which supported single-underscore-wrapping for italic and single-asterisk-wrapping for bold. However, this isn't
-                // part of the CommonMark specification that Markdig (that I now use supports and so we need to do this). We can ignore the DelimiterCount value and just look at the DelimiterChar when deciding what to do.
-                return obj.DelimiterChar switch
-                {
-                    '*' => "strong",
-                    '_' => "em",
-                    _ => null,
-                };
-            }
 		}
 	}
 }
