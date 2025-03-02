60180420e2cef88f56e672e7d8a44e82790df933
Dan Roberts <dangger36@gmail.com>
2025-03-02 23:01:23 +0000

    Try updating GA

--- Blog/Misc/Constants.cs
+++ Blog/Misc/Constants.cs
@@ -5,7 +5,7 @@
 		/// <summary>
 		/// This is optional (and so may be null or blank), if not specified then Google Analytics functionality will be disabled
 		/// </summary>
-		public const string GoogleAnalyticsId = "UA-32312857-1";
+		public const string GoogleAnalyticsId = "G-WRKSEZTFBP";
 
 		/// <summary>
 		/// This is optional (and so may be null or blank), if not specified then Disqus Commenting functionality will be disabled
