f563a504a62e3a97da7ac96e4f8515dc9d121470
Dan Roberts <dangger36@gmail.com>
2025-03-19 18:19:12 +0000

    Tidy up some old "http://" -> "https://" references

--- Blog/Misc/Constants.cs
+++ Blog/Misc/Constants.cs
@@ -20,11 +20,11 @@
 		/// <summary>
 		/// This is optional (and so may be null or blank), if not specified then Twitter card meta data will not appear on Post URLs
 		/// </summary>
-		public const string TwitterImage = "http://www.productiverage.com/Content/Images/Grouch.jpg";
+		public const string TwitterImage = "https://www.productiverage.com/Content/Images/Grouch.jpg";
 
 		/// <summary>
 		/// This is optional (and so may be null or blank), if not specified then pages will not have a canonical link inserted
 		/// </summary>
-		public const string CanonicalLinkBase = "http://www.productiverage.com";
+		public const string CanonicalLinkBase = "https://www.productiverage.com";
 	}
 }
