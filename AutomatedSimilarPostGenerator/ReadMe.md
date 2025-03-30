bdec2074bdae74e5fb06886d496a5f21c33c579f
Dan Roberts <dangger36@gmail.com>
2025-03-30 18:07:36 +0100

    Tidy up the solution folder structure, and add ReadMe files to the assistance projects

---
+++ AutomatedSimilarPostGenerator/ReadMe.md
@@ -0,0 +1,5 @@
+# Automated Similar Post Generator
+
+Use the [Catalyst](https://github.com/curiosity-ai/catalyst) NuGet package to suggest similar posts (so that when I write a new blog post, I can run this and it may make suggestions of other posts to link to the new one and/or suggestions of existing "related posts" links that it might be better to update in light of the new post).
+
+See my post about it here: [Automating "suggested / related posts" links for my blog posts](https://www.productiverage.com/automating-suggested-related-posts-links-for-my-blog-posts).
--- ProofReader/Program.cs
+++ ProofReader/Program.cs
@@ -18,7 +18,7 @@ namespace ProofReader
     /// <summary>
     /// This was written when I realised how many spelling mistakes had slipped through the cracks over the years in my blog posts - the
     /// premise is to identify them and suggest corrections (potentially going as far as automatically correcting them in the future,
-    /// pendin a manual review before committing).
+    /// pending a manual review before committing).
     /// 
     /// Challenges include the large technical vocabulary (which has many words not found in common English dictionaries) and the content
     /// of code samples (for the purposes of this, code samples are skipped entirely).
